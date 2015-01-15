cc5a6cc074858d841cbdbe4fb54904a414ab5873
Daniel K. Lin <yettie.lin@gmail.com>
2015-01-15 21:28:18 +0800

    fix the patent scope link location

--- app/controllers/patents_controller.rb
+++ app/controllers/patents_controller.rb
@@ -1,13 +1,13 @@
 class PatentsController < ApplicationController
 
 
-	def index 
+	def index
     @patents = Patent.all.order("created_at DESC")
-	end 
+	end
 
 	def new
 
-		@patent = Patent.new 
+		@patent = Patent.new
 
 	end
 
@@ -24,7 +24,7 @@ class PatentsController < ApplicationController
 		end
 
 	end
-  
+
   def slice_sentence
     @patent = Patent.friendly.find(params[:id])
 		@patent_scopy_by_item = @patent.patent_scope.split("<br>")
@@ -37,11 +37,11 @@ class PatentsController < ApplicationController
       article = Article.new
       article.patent_id = params[:id].to_s
       #article.sentence_id = index+1
-      article.sentence = item 
+      article.sentence = item
 
       b.clear
       c.clear
-          if (item.index("<td class=\"rectd2\">")) == 0 
+          if (item.index("<td class=\"rectd2\">")) == 0
             b = item.slice(19..item.length).scan(/\d+/)
           else
             b = item.scan(/\d+/)
@@ -49,82 +49,82 @@ class PatentsController < ApplicationController
 
          # binding.pry
 
-          if b.size >=2           
+          if b.size >=2
             article.child_id = b[0]
-            article.parent_id = b[1] 
+            article.parent_id = b[1]
             article.sentence_id = b[0]
-          else 
+          else
             article.child_id = "no_child"
             article.parent_id = "no_parent"
             article.sentence_id = b[0]
-          end        
-	    
-      if article.save 
+          end
+
+      if article.save
       #  flash[:success] = "查詢成功!寫入資料庫成功!"
       #  redirect_to :back
       else
         puts "error occur"
       end
 	  end
-    redirect_to @patent  
+    redirect_to @patent
 
   end
-  
+
   def json_tree
     @patent = Patent.friendly.find(params[:id])
 		@patent_scopy_by_item = @patent.patent_scope.split("<br>")
-  
+
     @abc = Patent.relationship_parent(@patent_scopy_by_item)
     @def = Patent.relationship(@patent_scopy_by_item)
    # binding.pry
-    @show_index = @abc.select {|k,v| v.empty? } 
+    @show_index = @abc.select {|k,v| v.empty? }
 
     @result_hash = []
-      @show_index.keys.each do |root| 
+      @show_index.keys.each do |root|
      # binding.pry
-      @result_hash << build_tree(root.to_s, @def)  
-     # binding.pry    
+      @result_hash << build_tree(root.to_s, @def)
+     # binding.pry
       end
-     
+
     respond_to do |format|
-      format.json { render json: @result_hash.to_json  } 
+      format.json { render json: @result_hash.to_json  }
     end
   end
-  
+
 
 	def show
 
 	#	@patent = Patent.find(params[:id])
-	#   use friendly_id as slugged title 
+	#   use friendly_id as slugged title
 		@patent = Patent.friendly.find(params[:id])
-		@patent_scopy_by_item = @patent.patent_scope.split("<br>")      
+		@patent_scopy_by_item = @patent.patent_scope.split("<br>")
     if @patent.patent_scope.include? "<a href="
       render :template => "patents/manual"
     else
       @first_key = Keyworda.where.not('keyworda' => nil).order('priority asc').pluck(:keyworda)
       @second_key = Keywordb.where.not('keywordb' => nil).order('priority asc').pluck(:keywordb)
-      @patent_scopy_by_item_right = Array.new 
+      @patent_scopy_by_item_right = Array.new
       @repeat_no = Array.new(1,"No.0")
-      @patent.update_attributes(article_total: @patent_scopy_by_item.length) if @patent.article_total == nil 
+      @patent.update_attributes(article_total: @patent_scopy_by_item.length) if @patent.article_total == nil
     end
 	end
-  
+
 	def edit
 
 		#@patent = Patent.find(params[:id])
 		@patent = Patent.friendly.find(params[:id])
 		@patent_scopy_by_item = @patent.patent_scope.split("<br>")
 
-	end 
+	end
 
 	def update
 
 		@patent = Patent.friendly.find(params[:id])
 		return_patent_scope(params[:apply_no],params[:new_fetch_no])
-		
+
 		if @patent.update_attributes(patent_scope: @result)
 		redirect_to @patent
-		else 
+		else
 			puts "error occur"
 		end
 
@@ -154,27 +154,27 @@ class PatentsController < ApplicationController
 		result_page = form.submit(form.button_with(:name => "_IMG_檢索@s"))
 
 		#按下“專利範圍”的連結
-     
+
 		if !(result_page.links_with(:text => "")[3].href.match(/^http:\/\/twpat/) )
 			@result = "查無此專利案"
 		else
-		result_page2 = result_page.links_with(:text => "")[3].click
+		result_page2 = result_page.links_with(:text => "")[2].click
 		page_html = Nokogiri::HTML.parse(result_page2.parser.to_html)
-     
+
      while fetch_key >=0 do
 	#@result = page_html.xpath("html/body/form/table/tr[2]/td/table/tr/td/table/tr[3]/td/table/tr/td/table/tr[2]/td/table/tr/td[1]/div/table/tr[#{fetch_key}]/td[2]").to_s
    @result = page_html.xpath("/html/body/form/table/tr/td/table/tr[3]/td/table/tr/td/table/tr[3]/td/table/tr/td/table/tr[2]/td/table/tr/td[1]/div/table/tr[#{fetch_key}]/td[2]").to_s
       #binding.pry
-      if @result == "" 
-        fetch_key -= 1 
+      if @result == ""
+        fetch_key -= 1
       else
         break
-      end  # if statement end 
+      end  # if statement end
      end   # while loop end
     end # if statement end
-  # binding.pry 
+  # binding.pry
 
-    end # method end 
+    end # method end
 
 
 
@@ -188,7 +188,7 @@ class PatentsController < ApplicationController
   def build_tree(id, src)
     child_array = src[:"#{id}"]
   #  @index+=1
-    return { "name" => id } if child_array.empty? 
+    return { "name" => id } if child_array.empty?
   #  binding.pry
     #children = child_array
     return { "name" => id, "children" => child_array.map { |child_id| build_tree(child_id.to_s, src) } }
